a1fc0886e38f6d0d0dfcca700e496b697dd25e8e
David Orson <onlyorson@outlook.com>
2020-12-08 23:48:02 +0000

    Code

--- src/components/Code.js
+++ src/components/Code.js
@@ -26,13 +26,10 @@ const Code = () => {
       {/* {open ? ( */}<div className="code-dropdown">
         <p className="code-tagline">The code used for the functionality of this challenge is:</p>
         <div className="code-container">
-          <p><span className="yellow">const</span><span className="cyan"> PalindroneTest</span> =<span className="purple"> (string)</span><span className="yellow"> ={">"}</span> <span className="purple">{"{"}</span><br/> 
-            <span className="yellow indent">if</span><span className="cyan">(</span><span className="purple">string</span><span className="cyan">) {'{'}</span><br/>
-            <span className="yellow indent2">let </span><span className="purple">reversed </span>= <span className="yellow">(</span><span className="purple">string</span>.<span className="cyan">split</span><span className="purple">(</span><span className="orange">''</span><span className="purple">)</span>.<span className="cyan">reverse</span><span className="purple">()</span>.<span className="cyan">join</span><span className="purple">(</span><span className="orange">''</span><span className="purple">)</span><span className="yellow">)</span>;<br/><br/>
-            <span className="cyan indent2">setPalindrone</span><span className="yellow">(</span><span className="purple">(</span><span className="cyan">(</span><span className="purple">reversed </span><span className="yellow">=== </span><span className="purple">string</span>.<span className="cyan">toString()</span><span className="purple">)</span><span className="yellow">)</span>;<br/>
-            <span className="cyan indent2">{'}'}</span><span className="yellow"> else </span><span className="cyan">{'{'}</span><br />
-            <span className="cyan indent2">setPalindrone</span><span className="yellow">(</span><span className="orange">'error'</span><span className="yellow">)</span><br/>
-            <span className="cyan indent">{'}'}</span><br />
+          <p><span className=""></span>
+            <span className="yellow">const</span><span className="cyan"> reverser</span> =<span className="purple"> (int)</span><span className="yellow"> ={">"}</span> <span className="purple">{"{"}</span><br/> 
+            <span className="yellow indent">const</span><span className="purple"> reversed</span> =<span className="purple"> int<br/></span><span className="indent2">.</span><span className="cyan">toString()<br/></span><span className="indent2">.</span><span className="cyan">split(</span><span className="orange">''</span><span className="cyan">)<br/></span><span className="indent2">.</span><span className="cyan">reverse()<br/></span><span className="indent2">.</span><span className="cyan">join(</span><span className="orange">''</span><span className="cyan">)</span>;<br/><br/>
+            <span className="cyan indent">setReversedInt(<br/><span className="indent2">parseInt</span></span><span className="yellow">(</span><span className="purple">reversed</span><span className="yellow">)</span> *<span className="purple"> Math</span>.<span className="cyan">sign</span><span className="yellow">(</span><span className="purple">int</span><span className="yellow">)</span><span className="cyan"><br/><span className="indent">)</span></span>;<br/>
             <span className="purple">{'}'}</span>
           </p>
         </div>
